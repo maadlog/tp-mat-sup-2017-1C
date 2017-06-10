26e0e4417cb0952c5cc9a0e0f1c961277ef6089f
Martín Loguancio <martin.loguancio@gmail.com>
2017-06-10 14:02:45 -0300

    Update ASIC.m - Menú y construcción de G

--- ASIC.m
+++ ASIC.m
@@ -1,13 +1,76 @@
-#Carga del paquete
 pkg load control
+run("./PF.m");
 
-#Sistema de ejemplo
-G=tf(
-  [5,2], #Coeficientes P Numerador
-  [1,4,20] #Coeficientes Q Denominador
-  )
+function ret_var = getGFromPQ()
+	disp("Ingrese con el formato requerido [a1,a2,..,an]\n")
+	numerador = input("Coeficientes del polinomio Numerador > ")
+	denominador = input("Coeficientes del polinomio Denominador > ")
+	ret_var = tf(numerador,denominador)
+endfunction
 
-#Grafico de polos y zeros de ejemplo
-pzmap(G)
+function ret_var = getGfromPZK()
+  disp("Ingrese con el formato requerido [a1,a2,..,an]\n")
+	ceros = input("Ceros > ")
+	polos = input("Polos > ")  
+  disp("Ingrese un valor unico k\n")  
+	ganancia = input("Ganancia > ")
+	ret_var = zpk(ceros,polos,ganancia)
+endfunction
 
-input("Press any key to exit");
+function ret_var = promptG()
+
+	opt = menu ("Seleccione la forma en la que se proveerá el Sistema de Control que desea Analizar\n",
+		"Coeficientes de polinomios",
+		"Ceros, Polos y Ganancia");
+
+	if (opt == 1)
+		ret_var=getGFromPQ();
+	else
+	  	ret_var=getGfromPZK();
+	endif
+
+endfunction
+
+
+G = promptG();
+
+while(true)
+
+	opt = menu ("Indique la operación a realizar\n",
+
+		"Expresar la función transferencia.",
+		"Indicar Polos.",
+		"Indicar Ceros.",
+		"Indicar Ganancia.",
+		"Obtener expresión con polos, ceros y ganancia.",
+		"Constelación de polos y ceros.",
+		"Estabilidad del sistema.",
+		"Obtener todas las características de la función transferencia",
+		"Ingresar una nueva función",
+		"Finalizar"
+	)
+
+	switch (opt)
+
+		case 1 #"Expresar la función transferencia."
+			disp("No implementado\n");
+		case 2 #"Indicar Polos."
+			disp("No implementado\n");
+		case 3 #"Indicar Ceros."
+			disp("No implementado\n");
+		case 4 #"Indicar Ganancia."
+			disp("No implementado\n");
+		case 5 #"Obtener expresión con polos, ceros y ganancia."
+			disp("No implementado\n");
+		case 6 #"Constelación de polos y ceros."
+			disp("No implementado\n");
+		case 7 #"Estabilidad del sistema."
+			disp("No implementado\n");
+		case 8 #"Obtener todas las características de la función transferencia"
+			disp("No implementado\n");
+		case 9 #"Ingresar una nueva función"
+			G=promptG();
+		otherwise #"Finalizar"
+			quit
+	endswitch
+endwhile
